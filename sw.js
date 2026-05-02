a0e605984e79eaae6bd98d2b5aed78f1fe2e5429
DieZeit1 <127352084+DieZeit1@users.noreply.github.com>
2026-05-02 13:44:03 +0400

    Results for "compact" view wriitng was edited.
    Do etogo pri vvedenii cifr okno sbegalo vverx

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agf-wrestling-pwa-v44';
+const CACHE_NAME = 'agf-wrestling-pwa-v45';
 const APP_SHELL = ['./', './index.html', './logo1.webp', './agf.webp'];
 
 self.addEventListener('install', (event) => {
